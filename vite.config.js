5fadbeb2ae90f09e5bc4d2c890fe5d74a5d38b66
Radithyasatya <radithyasatya7@gmail.com>
2026-01-14 14:46:12 +0700

    refactor: change config, no vite proxy

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://10.0.0.6:8080',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
